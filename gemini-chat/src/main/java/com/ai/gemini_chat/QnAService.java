26fab9ae00f1610dc38357f546073e278f15bccb
Rafael Calixto <calixxtorafaell@gmail.com>
2025-03-17 17:38:43 -0300

    feat(api): Add getAnswer method to fetch responses from Gemini API

--- gemini-chat/src/main/java/com/ai/gemini_chat/QnAService.java
+++ gemini-chat/src/main/java/com/ai/gemini_chat/QnAService.java
@@ -2,21 +2,42 @@ package com.ai.gemini_chat;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class QnAService {
-        //Access API key and URL to Gemini
+    // Access to APIKey and URL [Gemini]
     @Value("${gemini.api.url}")
     private String geminiApiUrl;
     @Value("${gemini.api.key}")
-    private String apiKey;
+    private String geminiApiKey;
 
-    public String getService(String question) {
-        //construct the request payload
+    private final WebClient webClient;
 
-        //api call
+    public QnAService(WebClient.Builder webClient) {
+        this.webClient = webClient.build();
+    }
 
-        //return response
-        return "";
+    public String getAnswer(String question) {
+        // request payload
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[] {
+                        Map.of("parts", new Object[] {
+                                Map.of("text", question)
+                        } )
+                }
+        );
+
+        String response = webClient.post()
+                .uri(geminiApiUrl + geminiApiKey)
+                .header("Content-Type","application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        return response;
     }
 }
